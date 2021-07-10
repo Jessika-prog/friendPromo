1a4d41138544f238323f215cb884b39dbdf5cf3f
Jessika-prog <58844655+Jessika-prog@users.noreply.github.com>
2021-07-10 12:25:47 +0200

    modif liste

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let streamer = [
     ['Astrelays', 'https://static-cdn.jtvnw.net/jtv_user_pictures/2ed9a754-6a50-4d30-be23-335ce7b58b6a-profile_image-150x150.png'],
     ['BobsLeague', 'https://static-cdn.jtvnw.net/jtv_user_pictures/faf10a68-6abd-45c6-9519-e954cb4f8ce1-profile_image-150x150.png'],
-    ['Boozook', 'https://static-cdn.jtvnw.net/jtv_user_pictures/d8b65043-9ad7-435f-927d-d93ec5302d95-profile_image-150x150.png'],
+    ['Boozook', 'https://static-cdn.jtvnw.net/jtv_user_pictures/100ac844-4d3d-486a-aa95-388e3cf397c5-profile_image-150x150.png'],
     ['CapitainManette', 'https://static-cdn.jtvnw.net/jtv_user_pictures/92f9f80d-658a-43c9-b141-3b59ebd05718-profile_image-150x150.png'],
+    ['CesXVI', 'https://static-cdn.jtvnw.net/jtv_user_pictures/fc21f1cc-d5f5-4441-b2cd-0bff0f2a3720-profile_image-150x150.png'],
     ['DrBynw', 'https://static-cdn.jtvnw.net/jtv_user_pictures/3d13d40e-fcb7-40ab-97f7-00c7a70ca6d7-profile_image-150x150.png'],
     ['GlayTV', 'https://static-cdn.jtvnw.net/jtv_user_pictures/d85bfec4-2069-4989-8f25-0cc91ebe77a6-profile_image-150x150.png'],
     ['Hastreed', 'https://static-cdn.jtvnw.net/jtv_user_pictures/2ed2dc28-2d66-4170-99d9-136f3ce50f6f-profile_image-150x150.png'],
@@ -12,10 +13,11 @@ let streamer = [
     ['Lomaxx222', 'https://static-cdn.jtvnw.net/jtv_user_pictures/97881b66-599d-4da5-b1e7-37a0138a2ff4-profile_image-150x150.png'],
     ['MakerVT', 'https://static-cdn.jtvnw.net/jtv_user_pictures/makervt-profile_image-078fb74aa83db4be-150x150.jpeg'],
     ['MayNCo', 'https://static-cdn.jtvnw.net/jtv_user_pictures/24e82107-1358-425e-9562-a721973c285c-profile_image-150x150.jpg'],
-    ['MonsieurDuc', 'https://static-cdn.jtvnw.net/jtv_user_pictures/monsieurduc-profile_image-f566a05af62151aa-150x150.jpeg'],
     ['Mr_krissou', 'https://static-cdn.jtvnw.net/jtv_user_pictures/b1ef5805-2ad7-4d98-a50e-9f33cd2a91f9-profile_image-150x150.png'],
+    ['Neburix', 'https://static-cdn.jtvnw.net/jtv_user_pictures/304a3561-6bf3-4be7-9610-f89acc9c2035-profile_image-150x150.png'],
     ['Nekfla', 'https://static-cdn.jtvnw.net/jtv_user_pictures/2016f33d-3998-47d4-8a8e-e7ffe03b926f-profile_image-150x150.png'],
     ['NinjHack', 'https://static-cdn.jtvnw.net/jtv_user_pictures/f2a7aea5-0485-405e-8af5-0c9e145015f2-profile_image-150x150.png'],
+    ['Pawm__', 'https://static-cdn.jtvnw.net/jtv_user_pictures/c65de358-54d9-4433-8240-afaa316da156-profile_image-150x150.png'],
     ['Pompowpidow', 'https://static-cdn.jtvnw.net/jtv_user_pictures/1b583dc1-a428-4a84-ac49-1402e6219f07-profile_image-150x150.jpeg'],
     ['Protoxhide_', 'https://static-cdn.jtvnw.net/jtv_user_pictures/0d4678f1-4c5c-419a-b65d-9d79a129a457-profile_image-150x150.png'],
     ['REC_FAMILY', 'https://static-cdn.jtvnw.net/jtv_user_pictures/7dcf1db6-a671-4186-8066-4bbbae75fc94-profile_image-150x150.png'],
@@ -33,7 +35,7 @@ const logo = document.querySelector('img');
 const pseudo = document.querySelector('.pseudo');
 
 document.addEventListener("DOMContentLoaded", (event) => {
-carte.setAttribute("class","carte slide")
+    carte.setAttribute("class", "carte slide")
     let i = 0;
     setInterval(() => {
         pseudo.innerHTML = streamer[i][0];
