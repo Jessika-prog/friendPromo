7b2b67ed782d2b8d11594946aceca52829189a19
Tsyka <58844655+Tsyka-prog@users.noreply.github.com>
2022-08-01 19:03:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let streamer = [
-    ['BobsLeague', 'https://static-cdn.jtvnw.net/jtv_user_pictures/91893193-aeb6-4993-a652-faf828e176ba-profile_image-150x150.png'],
-    ['Boozook', 'https://static-cdn.jtvnw.net/jtv_user_pictures/100ac844-4d3d-486a-aa95-388e3cf397c5-profile_image-150x150.png'],
-    ['HerazorR', 'https://static-cdn.jtvnw.net/jtv_user_pictures/e251ea13-98fe-45e8-8a87-aeaafadcc40c-profile_image-150x150.png'],
-    ['Mr_krissou', 'https://static-cdn.jtvnw.net/jtv_user_pictures/b1ef5805-2ad7-4d98-a50e-9f33cd2a91f9-profile_image-150x150.png'],
-    ['NinjHack', 'https://static-cdn.jtvnw.net/jtv_user_pictures/f2a7aea5-0485-405e-8af5-0c9e145015f2-profile_image-150x150.png'],
+    ['BobsLeague', 'https://static-cdn.jtvnw.net/jtv_user_pictures/91893193-aeb6-4993-a652-faf828e176ba-profile_image-150x150.png','Hearthstone BG'],
+    ['Boozook', 'https://static-cdn.jtvnw.net/jtv_user_pictures/100ac844-4d3d-486a-aa95-388e3cf397c5-profile_image-150x150.png','MultiGaming'],
+    ['HerazorR', 'https://static-cdn.jtvnw.net/jtv_user_pictures/e251ea13-98fe-45e8-8a87-aeaafadcc40c-profile_image-150x150.png','MultiGaming'],
+    ['Mr_krissou', 'https://static-cdn.jtvnw.net/jtv_user_pictures/b1ef5805-2ad7-4d98-a50e-9f33cd2a91f9-profile_image-150x150.png', '???'],
+    ['NinjHack', 'https://static-cdn.jtvnw.net/jtv_user_pictures/f2a7aea5-0485-405e-8af5-0c9e145015f2-profile_image-150x150.png','MultiGaming'],
     ['OliviaLaDouceur', 'https://static-cdn.jtvnw.net/jtv_user_pictures/acc39a77-6cd3-44e3-ba73-b79316306260-profile_image-150x150.png'],
-    ['Pompowpidow', 'https://static-cdn.jtvnw.net/jtv_user_pictures/1b583dc1-a428-4a84-ac49-1402e6219f07-profile_image-150x150.jpeg'],
-    ['Renantelle', 'https://static-cdn.jtvnw.net/jtv_user_pictures/f580e4f0-0734-4fb5-9cc9-7242c07c6778-profile_image-150x150.png'],
-    ['Superlicorneman', 'https://static-cdn.jtvnw.net/jtv_user_pictures/dcf853f7-c709-45db-bdcd-a4112df846d6-profile_image-150x150.png'],
-    ['VisioTempus', 'https://static-cdn.jtvnw.net/jtv_user_pictures/274d022e-365a-4595-a5d3-7d6bdfccc610-profile_image-150x150.png'],
-    ['Yssakam', 'https://static-cdn.jtvnw.net/jtv_user_pictures/0f42d1d6-076e-46da-a30a-40cd5042759a-profile_image-150x150.png']];
+    ['Pompowpidow', 'https://static-cdn.jtvnw.net/jtv_user_pictures/1b583dc1-a428-4a84-ac49-1402e6219f07-profile_image-150x150.jpeg','JustDance Queen'],
+    ['Renantelle', 'https://static-cdn.jtvnw.net/jtv_user_pictures/f580e4f0-0734-4fb5-9cc9-7242c07c6778-profile_image-150x150.png','MultiGaming'],
+    ['Superlicorneman', 'https://static-cdn.jtvnw.net/jtv_user_pictures/dcf853f7-c709-45db-bdcd-a4112df846d6-profile_image-150x150.png','MultiGaming'],
+    ['VisioTempus', 'https://static-cdn.jtvnw.net/jtv_user_pictures/274d022e-365a-4595-a5d3-7d6bdfccc610-profile_image-150x150.png','MultiGaming'],
+    ['Yssakam', 'https://static-cdn.jtvnw.net/jtv_user_pictures/0f42d1d6-076e-46da-a30a-40cd5042759a-profile_image-150x150.png','MultiGaming']];
 
 
 
